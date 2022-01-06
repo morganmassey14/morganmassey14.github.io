9160f74d8f1975101b0af9b54b1f454a6992fe01
tgann11 <morganne1170@gmail.com>
2022-01-06 15:48:40 -0600

    preparing for media queries

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,14 +1,51 @@
 import React from "react";
 import profile from "../images/profile.svg";
+import { BsLinkedin, BsGithub, BsFillEnvelopeFill } from "react-icons/bs";
 
 export const Description = () => {
     return (
         <>
         <div id="description" className="description">
-            <div className="description-section__about">
+
+        <section className="description_text"> <div className="description-section__about">
             <h1>Howdy, Im Morgan Massey. A UI/UX Designer & Front-End Developer</h1>
+            </div></section>
+        <section>   
+             <div className="profilepicture"><img className="profile-img" src={profile} alt="mmpic" /></div>
+            
+            <div className="icon-container">
+            <div className="description__icon">
+                <a href="https://www.linkedin.com/in/morganelizmassey/"
+                target="_blank"
+                rel="noreferrer"
+                title="Visit LinkedIn">
+                    <BsLinkedin />
+
+                </a>
             </div>
-            <div className="profilepicture"><img className="profile-img" src={profile} alt="mmpic" /></div>
+
+            <div className="description__icon">
+                <a href="https://github.com/morganmassey14"
+                target="_blank"
+                rel="noreferrer"
+                title="Visit GitHub">
+                    <BsGithub />
+
+                </a>
+            </div>
+
+            <div className="description__icon">
+                <a href={"mailto:" + "morganelizmassey@gmail.com" }
+                target="_blank"
+                rel="noreferrer"
+                title="Send Email">
+                    <BsFillEnvelopeFill />
+
+                </a>
+            </div>
+            </div>
+            </section>
+
             </div>
             </>
     )
